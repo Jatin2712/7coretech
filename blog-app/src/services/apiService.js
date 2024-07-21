59b1c3e120d7c4d646fabbac2d8e0047af68ec99
Jatin2712 <jk125771@gmail.com>
2024-07-21 19:14:12 +0530

    blop-app

--- blog-app/src/services/apiService.js
+++ blog-app/src/services/apiService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
+//const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://7coretech-xi.vercel.app/";
 
 export const fetchPosts = async (page, pageSize, category) => {
   try {
